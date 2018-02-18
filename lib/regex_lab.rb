53dc513450fc624c9547a8f165d4c2d90185732e
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-18 02:46:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,9 @@
 def starts_with_a_vowel?(word)
   if word.match(/\A[aeiou]/) == nil
     return false
-  else true
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
