2d518ae9bbf5db55517350d2bbda0d7b6ffd5d12
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-18 03:05:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiouAEIOU]/)
-    true
-  else false
-  end
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
